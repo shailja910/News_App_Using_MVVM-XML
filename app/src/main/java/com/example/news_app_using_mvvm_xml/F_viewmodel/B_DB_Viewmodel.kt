897cd362aed3f7189bc582ea031b7b7d659a2219
Shailja Sharma <shailjasharma91@yahoo.com>
2025-06-02 14:09:49 -0600

    Click fab button to save article on room DB

--- app/src/main/java/com/example/news_app_using_mvvm_xml/F_viewmodel/B_DB_Viewmodel.kt
+++ app/src/main/java/com/example/news_app_using_mvvm_xml/F_viewmodel/B_DB_Viewmodel.kt
@@ -6,14 +6,7 @@ import com.example.news_app_using_mvvm_xml.I_repository.B_Db_Repository
 import kotlinx.coroutines.launch
 
 class B_DB_Viewmodel(private val repository: B_Db_Repository) : ViewModel() {
-
-    val allArticles: LiveData<List<Article>> = repository.allArticles
-
     fun insert(article: Article) = viewModelScope.launch {
         repository.insert(article)
-    }
-
-    fun delete(article: Article) = viewModelScope.launch {
-        repository.delete(article)
     }
 }
--- app/src/main/java/com/example/news_app_using_mvvm_xml/I_repository/B_Db_Repository.kt
+++ app/src/main/java/com/example/news_app_using_mvvm_xml/I_repository/B_Db_Repository.kt
@@ -4,14 +4,7 @@ import com.example.news_app_using_mvvm_xml.B_model.Article
 import com.example.news_app_using_mvvm_xml.G_RoomDB.ArticleDao
 
 class B_Db_Repository(private val dao: ArticleDao) {
-
-    val allArticles = dao.getAllArticles()
-
     suspend fun insert(article: Article) {
         dao.insert(article)
-    }
-
-    suspend fun delete(article: Article) {
-        dao.delete(article)
     }
 }
